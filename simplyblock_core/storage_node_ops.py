edf57a21d5700684725d71221c6a254b7e5f7353
Max Schettler <max@simplyblock.io>
2025-03-21 16:15:59 +0100

    Deduplicate addNvmeDevices()

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -82,12 +82,7 @@ def _get_if_ip_address(ifname):
     logger.error("IP not found for interface %s", ifname)
     exit(1)
 
-
-def addNvmeDevices(snode, devs):
-    rpc_client = RPCClient(
-        snode.mgmt_ip, snode.rpc_port,
-        snode.rpc_username, snode.rpc_password, timeout=60, retry=10)
-
+def addNvmeDevices(rpc_client, snode, devs):
     devices = []
     ret = rpc_client.bdev_nvme_controller_list()
     ctr_map = {}
@@ -109,10 +104,6 @@ def addNvmeDevices(snode, devs):
             pci_st = str(pcie).replace("0", "").replace(":", "").replace(".", "")
             nvme_controller = "nvme_%s" % pci_st
             nvme_bdevs, err = rpc_client.bdev_nvme_controller_attach(nvme_controller, pcie)
-            # time.sleep(1)
-
-        if not nvme_bdevs:
-            continue
 
         for nvme_bdev in nvme_bdevs:
             rpc_client.bdev_examine(nvme_bdev)
@@ -1289,7 +1280,7 @@ def add_node(cluster_id, node_ip, iface_name, data_nics_list,
     node_info, _ = snode_api.info()
 
     # discover devices
-    nvme_devs = addNvmeDevices(snode, node_info['spdk_pcie_list'])
+    nvme_devs = addNvmeDevices(rpc_client, snode, node_info['spdk_pcie_list'])
     if nvme_devs:
 
         if not is_secondary_node:
@@ -1797,16 +1788,8 @@ def restart_storage_node(
 
     node_info, _ = snode_api.info()
 
-    if snode.is_secondary_node:
-        pass
-        # ret = _prepare_cluster_devices_on_restart(snode)
-        # if not ret:
-        #     logger.error("Failed to prepare cluster devices")
-        #     return False
-
-    else:
-
-        nvme_devs = addNvmeDevices(snode, node_info['spdk_pcie_list'])
+    if not snode.is_secondary_node:
+        nvme_devs = addNvmeDevices(rpc_client, snode, node_info['spdk_pcie_list'])
         if not nvme_devs:
             logger.error("No NVMe devices was found!")
             return False
